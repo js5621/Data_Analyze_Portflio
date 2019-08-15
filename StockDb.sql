9cadeaa78b1535ac05d7876b525418d47b83ef6e
js5621 <asaph5621@gmail.com>
2019-08-15 23:30:15 +0900

    remove StockDb.sql

--- StockDb.sql
+++
@@ -1,8 +0,0 @@
-create table board_collecting(
-   WriteDate varchar(70) not null primary key,
-   Title varchar(70) ,
-   contents varchar(3000),
-   ripple varchar(4000)) engine=InnoDb default character set= utf8;
-   delete from board_collecting;
-    SET SQL_SAFE_UPDATES = 0;
-select * from board_collecting 
